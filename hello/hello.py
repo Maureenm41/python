862d1702056ee508e5f8d2b6609f6022e4d39222
Maureenm41 <maureenmrchl@gmail.com>
2021-06-18 12:15:49 +0200

    up

--- hello/hello.py
+++ hello/hello.py
@@ -30,3 +30,30 @@ made up of multiple lines and sentences."""
 print(word)
 print(sentence)
 print(paragraph)
+
+2name = "James"
+-name = "Bond"
+My name = "Bond"
+
+name = "Bond"
+print("Your name is", name)
+
+last_name = "Bond"
+first_name = "James"
+text = "My name is {}, {} {}.".format(last_name, first_name, last_name)
+print(text)
+
+
+age = "26"
+first_name ="Maureen"
+text = "Hello, my name is {} and i am {}".format(first_name, age)
+print(text)
+
+first_name = "James" # String
+last_name = "Bond"  # String
+age = 39  # Integer
+weigth = 81.56 # Float
+double_agent = True # Boolean
+login = "007" # String
+agent = [first_name, last_name, age, weigth, double_agent, login] # List
+print(agent)

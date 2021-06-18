6073dec83c495a0c6cbef317c8d2644746859db8
Maureenm41 <maureenmrchl@gmail.com>
2021-06-18 12:16:17 +0200

    up

--- hello/hello.py
+++ hello/hello.py
@@ -57,3 +57,10 @@ double_agent = True # Boolean
 login = "007" # String
 agent = [first_name, last_name, age, weigth, double_agent, login] # List
 print(agent)
+
+
+print(first_name, type(first_name))
+print(last_name, type(last_name))
+print(age, type(age))
+print(weigth, type(weigth))
+print(agent, type(agent))

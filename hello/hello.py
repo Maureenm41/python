4715d7cc56922725ba24ee02f351dba8d35935d0
Maureenm41 <maureenmrchl@gmail.com>
2021-06-18 11:40:02 +0200

    up

--- hello/hello.py
+++ hello/hello.py
@@ -1,2 +1,28 @@
 msg = "Hello World"
 print(msg)
+
+def foo(x):
+    if x == 0:
+        bar()
+        baz()
+    else:
+        qux(x)
+        foo(x - 1)
+
+
+# My first comment 
+name = "BeCode" # Name of school
+
+"""
+This is a multiline
+comment.
+"""
+
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+print(word)
+print(sentence)
+print(paragraph)
